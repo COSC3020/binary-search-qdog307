8127543a1d367ebf52da82e03e146f78543212a6
Quincy Morris Reynolds <143751384+qdog307@users.noreply.github.com>
2024-09-17 11:37:05 -0600

    Search tree

--- code.js
+++ code.js
@@ -1,3 +1,19 @@
 function binarySearch(list, element) {
-    return -1;
+    let left = 0;
+    let right = list.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);  
+
+        if (list[mid] === element) {
+            return mid;  
+        } else if (list[mid] < element) {
+            left = mid + 1;  
+        } else {
+            right = mid - 1;  
+        }
+    }
+
+return -1;
+
 }
